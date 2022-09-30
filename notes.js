9571af9f51c2928cdc53e22244c10c879b7cf5e8
Kaarel Lainväe <113545783+klainvae@users.noreply.github.com>
2022-09-30 09:51:09 +0300

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,17 @@
+//create
+numbers=[1,2,3,8,10,6]
+console.log(numbers)
+
+
+//read
+console.log(numbers[2])
+
+
+//update
+numbers[1] = 5
+console.log(numbers)
+
+
+//delete
+numbers.splice(3, 1)
+console.log(numbers)
